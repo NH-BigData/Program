f82d77b5e8915b17fed79918f28022c64a53c892
김호성 <68679792+HoSeong0731@users.noreply.github.com>
2023-11-09 23:26:09 +0900

    Handler 구상

---
+++ AIT_CLIENT/Handler/SecureHandler/SecureObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handler.SecureHandler
+{   
+    public class SecureObject
+    {
+        private readonly byte[] key;
+
+        public SecureObject()
+        {
+            key = new[]
+            {
+                (byte) 'H',
+                (byte) 'O',
+                (byte) 'S',
+                (byte) 'E',
+                (byte) 'O',
+                (byte) 'N',
+                (byte) 'G',
+                (byte) '!'
+            };
+        }
+
+        public string GetEncryptString(string targetString)
+        {
+            return GetEncryptString(key, targetString);
+        }
+
+        private string GetEncryptString(byte[] key, string targetString)
+        {
+
+            return string.Empty;
+        }
+
+    }
+}
---
+++ AIT_CLIENT/Handler/SystemConfigHandler/DataBaseConfig.cs
@@ -0,0 +1,70 @@
+﻿using System.Diagnostics;
+using System.Text;
+using Handler.SecureHandler;
+
+namespace Handler.SystemConfigHandler
+{
+    public class DBConfig
+    {
+        private readonly IniHandler ini = new IniHandler();
+
+        public string this[string indexStr]
+        {
+            get
+            {
+                Trace.WriteLine(string.Format("DBConfig > get prop[{0}], INI: {1}", indexStr, ini.iniPath));
+                return ini.ReadIni("DATABASE", indexStr);
+            }
+            set
+            {
+                Trace.WriteLine(string.Format("DBConfig > set prop[{0}]={1}", indexStr, value));
+                ini.WriteIni("DATABASE", indexStr, value);
+            }
+        }
+
+        public DBConfig()
+        {
+
+        }
+
+        public DBConfig(string sysConfigPath)
+        {
+            ini = new IniHandler(sysConfigPath);
+        }
+
+        public string getConnectionString(string poolName)
+        {
+            var so = new SecureObject();
+            var vendor = this[poolName + "_VENDOR"];
+            var dbName = this[poolName];
+            var svrName = this[poolName + "_SVR"];
+            var port = this[poolName + "_PORT"];
+            var uid = this[poolName + "_UID"];
+            var pwd = this[poolName + "_PWD"];
+            try
+            {
+                pwd = so.getDESDecryptString(this[poolName + "_PWD"]);
+            }
+            catch
+            {
+                this[poolName + "_PWD"] = so.getDESEncryptString(pwd);
+            }
+
+            return GetConnectionString(vendor, dbName, svrName, port, uid, pwd);
+        }
+
+        public string GetConnectionString
+            (string vendor, string dbName, string svrName, string port, string uid, string pwd)
+        {
+            Trace.WriteLine(string.Format("DBConfig > getConnectionString({0}, {1}, {2} ...)", vendor, dbName,
+                svrName));
+
+            var ret = new StringBuilder("Data Source=").Append(svrName).Append(",").Append(port)
+                    .Append(";Initial Catalog=").Append(dbName)
+                    .Append(";User Id=").Append(uid)
+                    .Append(";Password=").Append(pwd).ToString();
+
+            return ret;
+        }
+    }
+}
