98e979ded054a11392eda10c426238c858f2a593
김호성 <68679792+HoSeong0731@users.noreply.github.com>
2023-11-10 22:37:10 +0900

    SQL Base 구현

---
+++ AIT_CLIENT/Handler/DataBaseHandler/SQLBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Handler.DataBaseHandler
+{
+    public class SQLBase
+    {
+        protected DBHandler handler;
+        public SQLBase(DBHandler handler)
+        {
+            this.handler = handler;
+        }
+
+        public SQLBase() : this(DBHandler.GetInstanse())
+        { 
+        }
+    }
+}
