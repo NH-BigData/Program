8e618c5a0b93d5af83aa696bb8a2d393ac48f7da
김호성 <68679792+HoSeong0731@users.noreply.github.com>
2023-11-12 19:15:27 +0900

    추상 class CRUD 추가

---
+++ AIT_CLIENT/Query/QueryCRUDBase.cs
@@ -0,0 +1,25 @@
+﻿namespace Query
+{
+    public abstract class QueryCRUDBase
+    {
+        public bool isAuditTable = true;
+
+        public QueryCRUDBase()
+        {
+            SetInsertSQL();
+            SetSelectSQL();
+            SetUpdateSQL();
+            SetDeleteSQL();
+        }
+
+        public string insertSqlText { get; set; }
+        public string selectSqlText { get; set; }
+        public string updateSqlText { get; set; }
+        public string deleteSqlText { get; set; }
+
+        protected abstract void SetInsertSQL();
+        protected abstract void SetSelectSQL();
+        protected abstract void SetUpdateSQL();
+        protected abstract void SetDeleteSQL();
+    }
+}
