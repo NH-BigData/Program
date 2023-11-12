46414751a68585badce36f61012245a91bf309f3
김호성 <68679792+HoSeong0731@users.noreply.github.com>
2023-11-12 21:22:21 +0900

    쿼리 읽을 함수들 추가 필요 (crud)

--- AIT_CLIENT/Handler/DataBaseHandler/SQLcrud.cs
+++ AIT_CLIENT/Handler/DataBaseHandler/SQLcrud.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Query;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,5 +10,24 @@ namespace Handler.DataBaseHandler
 
     public class SQLcrud : SQLBase
     {
+        public SQLcrud(QueryCRUDBase query) : this(DBHandler.GetInstanse(), query)
+        {
+        }
+
+        public SQLcrud(DBHandler handler, QueryCRUDBase query)
+            : base(handler)
+        {
+            insertSqlText = query.insertSqlText;
+            selectSqlText = query.selectSqlText;
+            updateSqlText = query.updateSqlText;
+            deleteSqlText = query.deleteSqlText;
+        }
+
+        public string insertSqlText { get; set; }
+        public string selectSqlText { get; set; }
+        public string updateSqlText { get; set; }
+        public string deleteSqlText { get; set; }
     }
+
+
 }
