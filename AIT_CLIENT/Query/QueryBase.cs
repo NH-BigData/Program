bfeb4dba972ef441c277256c04c731cd9716dd5a
김호성 <68679792+HoSeong0731@users.noreply.github.com>
2023-11-12 19:14:13 +0900

    추상 클래스 추가

---
+++ AIT_CLIENT/Query/QueryBase.cs
@@ -0,0 +1,28 @@
+﻿namespace Query
+{
+    public abstract class QueryBase
+    {
+        protected const string DYNAMIC_STR = "#Dynamic#";
+
+        public const string SQL_TYPE_SELECT = "_SELECT";
+        public const string SQL_TYPE_INSERT = "_INSERT";
+        public const string SQL_TYPE_UPDATE = "_UPDATE";
+        public const string SQL_TYPE_DELETE = "_DELETE";
+        public const string SQL_TYPE_PROCEDURE = "_PROCEDURE";
+
+        public QueryBase()
+        {
+            SetSQL();
+        }
+
+        public string sqlText { get; set; }
+        public string sqlType { get; set; }
+
+        protected abstract void SetSQL();
+
+        public void ApplyDynamicString(string value)
+        {
+            sqlText = sqlText.Replace(DYNAMIC_STR, value);
+        }
+    }
+}
